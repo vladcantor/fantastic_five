386cc16bf516e0d259b9e8c602279b09a01b5b09
Vlad Mihai Cantor <rapp93_rod@yahoo.com>
2016-06-24 22:25:45 +0300

    resurse

--- src/app/core/config.ts
+++ src/app/core/config.ts
@@ -2,8 +2,8 @@ declare let jQuery: any;
 
 export class ConfigService {
   config = {
-    name: 'sing',
-    title: 'Sing - Dashboard',
+    name: 'Cluj- All Inclusive',
+    title: 'Cluj- All Inclusive',
     version: '2.0.0',
     /**
      * Whether to print and alert some log information
